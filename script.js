c23adc8d9b1e45b93989d7ce910ac0da5c7fed65
virendra pratap singh yadav <146314825+virendrapratapsinghyadav@users.noreply.github.com>
2025-05-27 20:09:58 +0530

    details added inside circles

--- script.js
+++ script.js
@@ -25,27 +25,55 @@
     return isMatching;
  }
 
+
+
  async function fetchUserDetails(username) {
     const url = `https://leetcode-stats-api.herokuapp.com/${username}`
     try{
         searchButton.textContent = "Searching..";
         searchButton.disabled = true;
         const response = await fetch(url);
+        const parsedData = await response.json();
         if(!response.ok){
             throw new Error("Unable to fetch user data")
         }
-        const data = await response.json();
-        console.log("Logging data: ",data);
+        
+        console.log("Logging data: ",parsedData);
+        displayUserData(parsedData);
     }
+    
     catch(error){
-        statsContainer.innerHTML = `<p>Data not Found</p>`
+          statsContainer.innerHTML = `<p>Data not Found</p>`;
     }
+
     finally{
         searchButton.textContent = "Search";
         searchButton.disabled = false;
     }
  }
 
+
+ function displayUserData(userData){
+        const easySolved = userData.easySolved || 0;
+        const mediumSolved = userData.mediumSolved || 0;
+        const hardSolved = userData.hardSolved || 0;
+
+        const easytotal = userData.totalEasy; // avoid division by 0
+        const mediumtotal = userData.totalMedium; 
+        const hardtotal = userData.totalHard; 
+
+        const easyPercent = (easySolved / easytotal) * 100;
+        const mediumPercent = (mediumSolved / mediumtotal) * 100;
+        const hardPercent = (hardSolved / hardtotal) * 100;
+
+        easyProgressCircle.style.setProperty('--progress-degree', `${easyPercent}%`);
+        mediumProgressCircle.style.setProperty('--progress-degree', `${mediumPercent}%`);
+        hardProgressCircle.style.setProperty('--progress-degree', `${hardPercent}%`);
+
+        easyLabel.innerHTML = `Easy<br>${easySolved}/${easytotal}`;
+        mediumLabel.innerHTML = `Medium<br>${mediumSolved}/${mediumtotal}`;
+        hardLabel.innerHTML = `Hard<br>${hardSolved}/${hardtotal}`;
+ }
 
   searchButton.addEventListener('click', function(){
     const username = usernameInput.value;
